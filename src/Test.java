1e2cead3e9b577632d3521001a4e96e8c8373a78
Camilla Andersen <camilla92andersen@gmail.com>
2015-10-27 10:34:26 +0100

    Camilla trying to add for the first time

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,10 @@
 
 public class Test {
-	public static void main (String[]args){
+	public static void main (String[] args){
 		System.out.println("hello world");
 		System.out.println("hello to you too");
 		System.out.println("conflict");
 		System.out.println("Camilla's first commit");
 		System.out.println("Camilla's third commit");
-
 	}
 }
